6f26d0d665db776e1152947fddcd1b6098672626
PhenyoMore <96122658+PhenyoMore@users.noreply.github.com>
2024-12-03 15:29:53 +0200

    Update MTFOBRep.php

--- MTFOBRep.php
+++ MTFOBRep.php
@@ -1,4 +1,19 @@
 <?php
-$file = "C:\\Users\\PhenyoMore\\Evotel\\Andrew Cohoe - MTF - Planning\\011 - Reports\\Network Planning\\MTF Overbuild report.xlsx";
-shell_exec("start excel \"$file\"");
+// File: openFile.php
+
+// Mock user mapping
+$userFiles = [
+    'PhenyoMore' => 'C:\\Users\\PhenyoMore\\Evotel\\Andrew Cohoe - MTF - Planning\\011 - Reports\\Network Planning\\MTF Schools\\00 - Summary Report\\MTF School Tracker.xlsm',
+    'JohnDoe' => 'C:\\Users\\JohnDoe\\Evotel\\Andrew Cohoe - MTF - Planning\\011 - Reports\\Network Planning\\MTF Schools\\00 - Summary Report\\MTF School Tracker.xlsm'
+];
+
+$username = $_GET['user'] ?? null;
+
+if ($username && isset($userFiles[$username])) {
+    $filePath = $userFiles[$username];
+    shell_exec("start excel \"$filePath\"");
+    echo "File opened: $filePath";
+} else {
+    echo "Invalid user or file not found.";
+}
 ?>
